437fd9e44abdf4443b81ea6ddf88f17043fdd08b
farid-hashimzada <hashimzadafarid@gmail.com>
2024-01-25 05:46:18 -0800

    12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 100):
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print("{}".format(i), end=" ")
