4ceef44d704924e857eebb0ffbcac1a67bcb3ba9
farid-hashimzada <hashimzadafarid@gmail.com>
2024-01-25 03:58:37 -0800

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            cpy = chr(ord(i) - 32)
+        else:
+            cpy = i
+        print("{}".format(cpy), end="")
+    print("")
