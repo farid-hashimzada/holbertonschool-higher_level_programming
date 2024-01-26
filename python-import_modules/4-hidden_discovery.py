696c2b6ab65c7fbaeda32165cddfce837792ad1e
farid-hashimzada <hashimzadafarid@gmail.com>
2024-01-26 21:53:44 +0400

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,6 @@
 import hidden_4
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    sorted_names = sorted(name for name in names if not name.startswith("__"))
-    for name in sorted_names:
-        print(name)
-
+    for i in dir(hidden_4):
+        if i[0] != "_":
+            print(i)
