349438bbb93b62e9e0504d488df3713e4be66d54
farid-hashimzada <hashimzadafarid@gmail.com>
2024-01-28 18:57:21 +0400

    10-divisible_by_2.py

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return
+
+    bool_list = []
+
+    for i in my_list:
+        if i % 2 == 0:
+            bool_list.append(True)
+        else:
+            bool_list.append(False)
+    return bool_list
