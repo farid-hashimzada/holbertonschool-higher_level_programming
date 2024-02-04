3c1341e5911519a4fcd2939e4d77ffc5e12b542a
farid-hashimzada <hashimzadafarid@gmail.com>
2024-02-04 16:55:06 +0400

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""0-add_integer.py and tests/0-add_integer.txt"""
+
+
+def add_integer(a, b=98):
+    """
+    adds an integer
+    unit tests located in tests/0-add_integer.txt
+    checks for type errors
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
