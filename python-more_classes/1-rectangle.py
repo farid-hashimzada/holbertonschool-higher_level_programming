24ae3cdc2b3ac95d211dd8a0ca280079501c4b45
farid-hashimzada <hashimzadafarid@gmail.com>
2024-02-06 18:06:53 +0400

    0-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+    Rectangle Module
+'''
+
+
+class Rectangle:
+    '''
+        Rectangle Class
+    '''
+
+    pass
+
+def __int__(self, width = 0, height = 0):
+    self.__height = height
+    self.__width = width
+
+@property
+def width(self):
+    return self.__width
